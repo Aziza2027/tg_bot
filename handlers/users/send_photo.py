5017ac29685ff01a2d106e2967b9b8f5f67fb70c
Ismoilov Asasdbek <94944448+ismoilov299@users.noreply.github.com>
2022-04-22 16:07:21 +0500

    upgrade photos

--- handlers/users/send_photo.py
+++ handlers/users/send_photo.py
@@ -18,15 +18,25 @@ async def send_photo01(message: types.Message):
 @dp.message_handler(text_contains=("Umar ibn Xattob r.a"))
 async def send_photo01(message: types.Message):
     photo_file = InputFile(path_or_bytesio="photos/sahoba02.jpg")
-    await message.reply_photo(photo_file, caption="Abu Bakr As-Siddiq r.a haqida")
+    await message.reply_photo(photo_file, caption="Umar ibn Xattob (584-644)U Banu Adi qabilasidan bo'lgan. Umar ibn Xattob (r.a.) tarixda tanilgan eng qudratli va eng nufuzli xalifalardan biri bo'lgan.\n"
+                                                  " U Abu Bakrdan keyingi ikkinchi xalifa bo'lgan. Bu odamning ismi adolat va haqiqat, kuch va jasorat, zohidlik va xudojo'ylik bilan birlashtirilgan.U juda taqvodor edi. \n"
+                                                  "U Allohdan qo'rqar, va unga Alloh eslatilsa, yig'lab yuborar edi. U Rasululloh (s.a.v.)ning qayniotalaridan bir bo'lgan. \n"
+                                                  "Uning Islomni qabul qilishi da'vatning yangi bosqichini boshlab bergan.")
 
 @dp.message_handler(text_contains=("Usmon ibn Affon r.a"))
 async def send_photo01(message: types.Message):
     photo_file = InputFile(path_or_bytesio="photos/sahoba03.jpg")
-    await message.reply_photo(photo_file, caption="Abu Bakr As-Siddiq r.a haqida")
+    await message.reply_photo(photo_file, caption="Usmon ibn Affon (577-656)Usmon ibn Affon (r.a.) Fil yilidan taxminan olti yil o'tgacha,\n "
+                                                  "Makka yaqinidagi Toif shahrida Qurayshning boy oilasida tug'ilgan. \n"
+                                                  "U butun hayoti davomida mehribon, saxiy odam edi va Islomni qabul qilguniga qadar ham muhtojlarga yordam uchun pul berib turardi.\n"
+                                                  "Usmonning Payg'ambar ﷺning ikki qiziga uylanishi unga 'Zun-Nurayn' (ikki nur egasi) taxallusini bergan.")
 
 @dp.message_handler(text_contains=("Ali ibn Abu Tolib r.a"))
 async def send_photo01(message: types.Message):
     photo_file = InputFile(path_or_bytesio="photos/sahoba04.jpg")
-    await message.reply_photo(photo_file, caption="Abu Bakr As-Siddiq r.a haqida")
+    await message.reply_photo(photo_file, caption="Ali ibn Abu Tolib (600-661)Ali ibn Abu Tolib (r.a.) Fil yilining 13-Rajab oyida tug'ilgan.\n"
+                                                  " Ali va payg'ambar ﷺ o'rtasidagi sevgi ilohiy edi. Chunki ularning umumiy jihatlari ko'p edi.\n"
+                                                  " U Rasululloh ﷺning barcha yurishlarida ishtirok etgan. \n"
+                                                  "Ali qo'rqmas va sodiq jangchi, o'ta taqvodor inson va mohir notiq edi.Ali ibn Abu Tolib (r.a.) mo'minlarning onasi, \n"
+                                                  "Rasululloh ﷺning suyukli qizlari Fotimai Zahroga uylangan.")
 
