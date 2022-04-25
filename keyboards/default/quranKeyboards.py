ac371cc7b4d7f69784700d5a97c108a24f9bff5c
§ <>
2022-04-25 16:39:26 +0500

    update

--- keyboards/default/quranKeyboards.py
+++ keyboards/default/quranKeyboards.py
@@ -3,8 +3,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 quranMenu = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text='001'),
-        ]
+            KeyboardButton(text='Fotiha surasi'),
+            KeyboardButton(text='Baqara surasi'),
+        ],
     ],
     resize_keyboard=True
 )
